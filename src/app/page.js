bf7023b11326ad9f0bc83b7cffd53acebf7fdeba
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-01-17 11:58:50 +0200

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,18 @@
-// import { Carousel } from "../components/Carousel";
+import { Carousel } from "../components/Carousel";
 import { Solution } from "../components/Solution";
 import WhyUs from "../components/WhyUs";
 import Advantages from "@/components/Advantages";
 import Good from "../components/Good";
 import { Contact } from "../components/Contact";
 import  Blog  from "@/components/Blog";
-// import carouselData from "@/data/carouselData.json"
+import carouselData from "@/data/carouselData.json"
 
-// const slides = carouselData.slides; 
+const slides = carouselData.slides; 
 
 export default function Home() {
   return (
     <div>
-      {/* <Carousel data={slides} />  */}
+      <Carousel data={slides} /> 
       <Solution/>
       <Advantages/>
       <Good/>
