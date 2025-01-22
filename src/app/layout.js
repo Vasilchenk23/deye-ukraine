ad987e56cc9635a1978dba9d6123e9561ba08378
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-01-22 11:36:01 +0200

    add google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,6 +46,14 @@ export default function RootLayout({ children }) {
         <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
         <meta name="google-site-verification" content="cp4VE_lxYv1rEvbWq16NBxWRiONajWChBBojyNSKs8M" />
       </head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16830451066"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-16830451066');
+      </script>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <main>{children}</main>
