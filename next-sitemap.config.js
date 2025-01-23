788796a0135b92b3734a0b69bb21c47864a116ca
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-01-23 13:26:33 +0200

    add sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://www.deyepower.com.ua/',  // Укажи свой домен
+    generateRobotsTxt: true, // Это автоматически создаст robots.txt
+    // Дополнительные настройки, если нужно:
+    changefreq: 'daily',  // Частота обновления
+    priority: 0.7,        // Приоритет для страниц
+  };
+  
