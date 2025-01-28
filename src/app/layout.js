9e5612854256a7912569c56b34ac481b0e1833d6
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-01-28 15:04:47 +0200

    add change

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -66,19 +67,7 @@ export default function RootLayout({ children }) {
         `}
         
       </Script>
-      <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-        >
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-W6J62NWJ');
-          `}
-        </Script>
+        <GoogleTagManager gtmId="GTM-GTM-W6J62NWJ" />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <main>{children}</main>
